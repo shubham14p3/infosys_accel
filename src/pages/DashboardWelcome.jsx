d9acf252d014921f6bece2107f79a53cbd2018c9
Shubham Raj <29100993+shubham14p3@users.noreply.github.com>
2025-03-28 14:28:09 +0530

    Update DashboardWelcome.jsx

--- src/pages/DashboardWelcome.jsx
+++ src/pages/DashboardWelcome.jsx
@@ -1,7 +1,7 @@
 // src/components/DashboardWelcome.jsx
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-
+import gif from '../assets/1.gif';
 const DashboardWelcome = () => {
     return (
         <Box
@@ -18,13 +18,13 @@ const DashboardWelcome = () => {
             }}
         >
             <img
-                src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG5hZXJwbmx5N2NybndvMnQzbTd4c2U5bnBhZmVnbTgzZXRwcjNxZiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YTbZzCkRQCEJa/giphy.gif"
+                src={gif}
                 alt="Welcome Animation"
-                width="250"
+                width="550"
                 style={{ marginBottom: '1rem', borderRadius: '10px' }}
             />
             <Typography variant="h4" sx={{ fontWeight: 300 }}>
-                Welcome to AOAAL
+                Welcome to AOCAT
             </Typography>
             <Typography variant="body1" sx={{ mt: 1, maxWidth: 400 }}>
                 Your centralized automation control panel. Get started by selecting an option from the sidebar.

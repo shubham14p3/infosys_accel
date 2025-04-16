3167888d789044d1a5185f32cb23a49fa843a15b
Shubham Raj <29100993+shubham14p3@users.noreply.github.com>
2025-04-16 14:32:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import SignInSide from "./components/SignInSide";
-import Dashboard from "./pages/Dashboard";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import SignInSide from './components/SignInSide';
+import Dashboard from './pages/Dashboard';
+import DashboardWelcome2 from './pages/DashboardWelcome2';
+import NotFound from './pages/NotFound';
+import ErrorBoundary from './components/ErrorBoundary';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/signin" element={<SignInSide />} />
-      <Route path="/dashboard/*" element={<Dashboard />} />
-      <Route path="/" element={<Navigate to="/signin" />} />
-      <Route path="*" element={<Navigate to="/signin" replace />} />
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/signin" element={<SignInSide />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />{' '}
+        <Route path="/" element={<SignInSide />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ErrorBoundary>
   );
 }
 

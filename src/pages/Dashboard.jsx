d5fc11256ca22738cbe99e61dbf1ceb93f5532e9
Shubham Raj <29100993+shubham14p3@users.noreply.github.com>
2025-04-16 14:36:46 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,22 +1,24 @@
 // src/components/Dashboard.jsx
-import { Routes, Route } from "react-router-dom";
-import PageSkeleton from "./PageSkeleton";
-import GenerateEventsPage from "./GenerateEventsPage";
-import ExecutePage from "./ExecutePage";
-import HistoryPage from "./HistoryPage";
-import DashboardWelcome from "./DashboardWelcome"; // 👈 import welcome
+import { Routes, Route } from 'react-router-dom';
+import PageSkeleton from './PageSkeleton';
+import GenerateEventsPage from './GenerateEventsPage';
+import ExecutePage from './ExecutePage';
+import HistoryPage from './HistoryPage';
+import DashboardWelcome from './DashboardWelcome';
+import DashboardWelcome2 from './DashboardWelcome2';
 
 const Dashboard = () => {
-    return (
-        <PageSkeleton>
-            <Routes>
-                <Route path="" element={<DashboardWelcome />} /> {/* Default route */}
-                <Route path="generate-events" element={<GenerateEventsPage />} />
-                <Route path="execute" element={<ExecutePage />} />
-                <Route path="history" element={<HistoryPage />} />
-            </Routes>
-        </PageSkeleton>
-    );
+  return (
+    <PageSkeleton>
+      <Routes>
+        <Route path="home2" element={<DashboardWelcome />} /> 
+        <Route path="" element={<DashboardWelcome2 />} /> {/* Default route */}
+        <Route path="generate-events" element={<GenerateEventsPage />} />
+        <Route path="execute" element={<ExecutePage />} />
+        <Route path="history" element={<HistoryPage />} />
+      </Routes>
+    </PageSkeleton>
+  );
 };
 
 export default Dashboard;

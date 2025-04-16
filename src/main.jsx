cb5a2495b4655dade3443d42dacb961403444276
Shubham Raj <29100993+shubham14p3@users.noreply.github.com>
2025-04-16 14:32:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-         <App />
+        <App />
+        <ToastContainer position="top-right" autoClose={3000} />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
